944df5e84cc612c2c2d926636f8b2ac9deab97ea
bseton11-YJKnqQ <bseton11@gmail.com>
2022-03-08 00:04:01 -0600

    deploying

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://localhost/userandthoughts', {
+connect(process.env.MONGODB_URI || 'mongodb://localhost/userandthoughts', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 module.exports = connection;
